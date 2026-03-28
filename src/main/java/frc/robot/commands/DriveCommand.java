d82ec709c8e573abf38d67930833590c4d6f842a
QUANTEX10959 <orhaniye.r.sports@gmail.com>
2026-03-28 09:30:37 +0300

    iki kontrolcu

--- src/main/java/frc/robot/commands/DriveCommand.java
+++ src/main/java/frc/robot/commands/DriveCommand.java
@@ -8,7 +8,7 @@ import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.RobotContainer;
 import frc.robot.subsystems.DriveSubsystem;
 
-//fff import com.ctre.phoenix.motorcontrol.ControlMode;
+//import com.ctre.phoenix.motorcontrol.ControlMode;
 
 public class DriveCommand extends Command
 {   
@@ -44,17 +44,18 @@ public class DriveCommand extends Command
         */
 
         
-        if (RobotContainer.kontrol.getRawButton(1)) {
-            driveSubsystem.setFirlatmaMotors(-1, 1);
-        } 
-        else if (RobotContainer.kontrol.getRawButton(3))
-        {
-            driveSubsystem.setFirlatmaMotors(-1, -1);
-        } 
-        else {
-            
-            driveSubsystem.setFirlatmaMotors(0, 0);
-        }
+     if (RobotContainer.firlatma.getRawButton(1)) {
+        driveSubsystem.setFirlatmaMotors(-1, 1);
+    }
+
+    if (RobotContainer.firlatma.getRawButton(3)) {
+        driveSubsystem.setFirlatmaMotors(-1, -1);
+    }
+
+    if (!RobotContainer.firlatma.getRawButton(1) && 
+        !RobotContainer.firlatma.getRawButton(3)) {
+        driveSubsystem.setFirlatmaMotors(0, 0);
+    }
 
         // swerweTORK
         /*if (RobotContainer.kontrol.getRawButton(2)) {
