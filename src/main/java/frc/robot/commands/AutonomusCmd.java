1ea61f650b0e7990d1e63201755a23f41a632364
QUANTEX10959 <orhaniye.r.sports@gmail.com>
2026-03-28 12:23:28 +0300

    ADS

--- src/main/java/frc/robot/commands/AutonomusCmd.java
+++ src/main/java/frc/robot/commands/AutonomusCmd.java
@@ -1,170 +1,76 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.Command;
-//import frc.robot.RobotContainer;
 import frc.robot.subsystems.DriveSubsystem;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
-public class AutonomusCmd extends Command
-{   
+public class AutonomusCmd extends Command {   
     private final DriveSubsystem driveSubsystem;
-
     private static double startTime = 0;
 
-    public AutonomusCmd(DriveSubsystem driveSubsystem)
-    {
+    public AutonomusCmd(DriveSubsystem driveSubsystem) {
         this.driveSubsystem = driveSubsystem;
         addRequirements(driveSubsystem);
     }
     
-  
     @Override
-    public void initialize() 
-    {
+    public void initialize() {
         startTime = Timer.getFPGATimestamp();
-        driveSubsystem.differentialDrive1.setSafetyEnabled(false);
+        // Eğer sisteminde tanımlıysa güvenlik modunu kapatabilirsin. 
+        // driveSubsystem.differentialDrive.setSafetyEnabled(false); 
     }
 
     @Override
-    public void execute() 
-    {    
+    public void execute() {    
         double time = Timer.getFPGATimestamp();
-        SmartDashboard.putNumber("Autonomous Time: ", time - startTime);
+        double elapsedTime = time - startTime;
+        SmartDashboard.putNumber("Autonomous Time: ", elapsedTime);
 
-        if (time - startTime < 1)
-        {   
-            driveSubsystem.differentialDrive.arcadeDrive(0, 1);
-        }
-        else
-        {
-            driveSubsystem.differentialDrive.arcadeDrive(0, 0);
-        }
-         
-        // if (time - startTime >= 0.5 && time - startTime < 2)
-        // {   
-        //    driveSubsystem.differentialDrive.arcadeDrive(0, 0);
-        // }
+        // ZAMAN ÇİZELGESİ (Süreleri ve Hızları robotunun ağırlığına göre sahada test ederek değiştirmelisin)
         
-        // if (time - startTime >= 2 && time - startTime < 2.5)
-        // {
-        //     if (driveSubsystem.getGyroAngle() < 90) {
-        //         driveSubsystem.differentialDrive.arcadeDrive(0, 0.5);
-        //     } else {
-        //         driveSubsystem.differentialDrive.arcadeDrive(0, 0);
-        //     }
-        // }
-        // if (time - startTime >= 2.5 && time - startTime < 10000)
-        // {
-        //     driveSubsystem.differentialDrive.arcadeDrive(0, 0);
-        // }
-        /*  
-        if (time - startTime >= 6 && time - startTime < 7)
-        {   
-            driveSubsystem.setDriveMotors(-0.5, -0.5, 0.5, 0.5);
-            driveSubsystem.setIntakeMotors(0, 0, 0);
-        } 
-        if (time - startTime >= 7)
-        {   
-            driveSubsystem.setDriveMotors(0, 0, 0, 0);
-            driveSubsystem.setIntakeMotors(0, 0, 0);
-        }     
-        */
-
-        // sadece firlatma
-        /* 
-        if (time - startTime < 2)
-        {   
-            driveSubsystem.setDriveMotors(0, 0, 0, 0);
-            driveSubsystem.setIntakeMotors(0.6, 0.6, 0.6);
-        } 
-        if (time - startTime >= 2 && time - startTime < 3)
-        {   
-            driveSubsystem.setDriveMotors(0, 0, 0, 0);
+        if (elapsedTime < 2.0) {
+            // 1. AŞAMA: Yeşil kutudan çıkış ve orta çizgiye kavisli yönelme
+            // Sola göre daha hızlı dönen sol motor, robotun sağa doğru kavis yapmasını sağlar.
+            driveSubsystem.differentialDrive.tankDrive(0.6, 0.4); 
             driveSubsystem.setIntakeMotors(0, 0, 0);
         }
-        if (time - startTime >= 3 && time - startTime < 5)
-        {
-            driveSubsystem.setDriveMotors(0, 0, 0, 0);
-            driveSubsystem.setIntakeMotors(-1, -1, -1);
+        else if (elapsedTime >= 2.0 && elapsedTime < 3.5) {
+            // 2. AŞAMA: Not'a (halkaya) doğru düz ilerleme ve Intake çalıştırma
+            driveSubsystem.differentialDrive.tankDrive(0.4, 0.4); 
+            driveSubsystem.setIntakeMotors(0.6, 0.6, 0.6); // Intake'i içeri alacak şekilde çalıştır
         }
-        if (time - startTime >= 5)
-        {
-            driveSubsystem.setDriveMotors(0, 0, 0, 0);
-            driveSubsystem.setIntakeMotors(0, 0, 0);
+        else if (elapsedTime >= 3.5 && elapsedTime < 4.0) {
+            // 3. AŞAMA: Notu tam olarak içeri aldığından emin olmak için kısa duraklama
+            driveSubsystem.differentialDrive.tankDrive(0, 0);
+            driveSubsystem.setIntakeMotors(0, 0, 0); 
         }
-        
-        /*
-        // kirmizi
-        if (time - startTime < 2)
-        {   
-            driveSubsystem.setDriveMotors(0, 0, 0, 0);
-            driveSubsystem.setIntakeMotors(0.5, 0.5, 0.5);
-        } 
-        if (time - startTime >= 2 && time - startTime < 3)
-        {   
-            driveSubsystem.setDriveMotors(0, 0, 0, 0);
+        else if (elapsedTime >= 4.0 && elapsedTime < 6.5) {
+            // 4. AŞAMA: Kırmızı kutuya (Subwoofer) geriye doğru kavis çizerek dönme
+            // Geri giderken sağ motorun daha hızlı (negatif olarak daha büyük) dönmesi yönünü ayarlar
+            driveSubsystem.differentialDrive.tankDrive(-0.4, -0.6);
             driveSubsystem.setIntakeMotors(0, 0, 0);
         }
-        if (time - startTime >= 3 && time - startTime < 5)
-        {
-            driveSubsystem.setDriveMotors(0, 0, 0, 0);
-            driveSubsystem.setIntakeMotors(-1, -1, -1);           
+        else if (elapsedTime >= 6.5 && elapsedTime < 8.0) {
+            // 5. AŞAMA: Dur ve Fırlat (Shooter)
+            driveSubsystem.differentialDrive.tankDrive(0, 0);
+            driveSubsystem.setIntakeMotors(-1.0, -1.0, -1.0); // Fırlatma yönünde motorları çalıştır
         }
-        if (time - startTime >= 5 && time - startTime < 6)
-        {   
-            driveSubsystem.setDriveMotors(-0.2, -0.2, 0.2, 0.2);
-            driveSubsystem.setIntakeMotors(0, 0, 0);
-        } 
-        if (time - startTime >= 6 && time - startTime < 7)
-        {
-            driveSubsystem.setDriveMotors(0, 0, 0, 0);
+        else {
+            // 6. AŞAMA: Otonom süresi bitti, her şeyi durdur
+            driveSubsystem.differentialDrive.tankDrive(0, 0);
             driveSubsystem.setIntakeMotors(0, 0, 0);
         }
-        if (time - startTime >= 7 && time - startTime < 8)
-        {   
-            driveSubsystem.setDriveMotors(-0.3, -0.3, -0.3, -0.3);
-            driveSubsystem.setIntakeMotors(0, 0, 0);
-        } 
-        if (time - startTime >= 8)
-        {   
-            driveSubsystem.setDriveMotors(0, 0, 0, 0);
-            driveSubsystem.setIntakeMotors(0, 0, 0);
-        }
-      
-        if (time - startTime >= 9 && time - startTime < 10)
-        {
-            driveSubsystem.setDriveMotors(0.4, 0.4, -0.4, -0.4);
-            driveSubsystem.setIntakeMotors(0, 0, 0);
-        }
-        if (time - startTime >= 10 && time - startTime < 11)
-        {
-            driveSubsystem.setDriveMotors(0, 0, 0, 0);
-            driveSubsystem.setIntakeMotors(0, 0, 0);
-        }
-        if (time - startTime >= 11 && time - startTime < 12)
-        {   
-            driveSubsystem.setDriveMotors(-0.2, -0.2, -0.2, -0.2);
-            driveSubsystem.setIntakeMotors(0, 0, 0);
-        } 
-        if (time - startTime >= 12)
-        {   
-            driveSubsystem.setDriveMotors(0, 0, 0, 0);
-            driveSubsystem.setIntakeMotors(0, 0, 0);
-        }
-        */
     }
 
     @Override
-    public void end(boolean interrupted) 
-    {
-        driveSubsystem.setDriveMotors(0, 0, 0, 0);
+    public void end(boolean interrupted) {
+        driveSubsystem.differentialDrive.tankDrive(0, 0);
         driveSubsystem.setIntakeMotors(0, 0, 0);
     }
 
     @Override
-    public boolean isFinished() 
-    {
+    public boolean isFinished() {
         return false;
     }   
 }
